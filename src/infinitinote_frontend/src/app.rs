a3e22d9802a2d6b4be733ff1d171f25614578dbd
Hafez <67077605+hafezrouzati@users.noreply.github.com>
2023-01-25 15:54:21 -0800

    update UI scaffold

--- src/infinitinote_frontend/src/app.rs
+++ src/infinitinote_frontend/src/app.rs
@@ -1,7 +1,7 @@
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(module="/assets/bridge.js")]
+#[wasm_bindgen]
 extern {
     fn greet(name: String);
 }
@@ -72,48 +72,59 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+        // egui::SidePanel::left("side_panel").show(ctx, |ui| {
+        //     ui.heading("Side Panel");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
+        //     // ui.horizontal(|ui| {
+        //     //     ui.label("Write something: ");
+        //     //     ui.text_edit_singleline(label);
+        //     // });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
+        //     // ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
+        //     // if ui.button("Increment").clicked() {
+        //     //     *value += 1.0;
+        //     // }
+
+        //     if ui.button("Greet").clicked() {
+        //         let g = greet("Hafez".to_string());
+        //         ui.label("greet");
+        //     }
+
+        //     ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+        //         ui.horizontal(|ui| {
+        //             ui.spacing_mut().item_spacing.x = 0.0;
+        //             ui.label("powered by ");
+        //             ui.hyperlink_to("egui", "https://github.com/emilk/egui");
+        //             ui.label(" and ");
+        //             ui.hyperlink_to(
+        //                 "eframe",
+        //                 "https://github.com/emilk/egui/tree/master/crates/eframe",
+        //             );
+        //             ui.label(".");
+        //         });
+        //     });
+        // });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+
+            ui.heading("infinitinote");
+            ui.hyperlink("https://github.com/hafezrouzati/infinitinote");
+            ui.add(egui::github_link_file!(
+                "https://github.com/hafezrouzati/infinitinote/",
+                "Source code."
+            ));
 
             if ui.button("Greet").clicked() {
                 let g = greet("Hafez".to_string());
                 ui.label("greet");
             }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
+            
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_multiline(label);
             });
-        });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
             egui::warn_if_debug_build(ui);
         });
 
