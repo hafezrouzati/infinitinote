b7f06527a9578c703753e190bb33840135c98246
godlikehira <0iamhira@gmail.com>
2023-06-23 08:45:03 +0545

    merging done

--- src/infinitinote_frontend/src/components/Signup.vue
+++ src/infinitinote_frontend/src/components/Signup.vue
@@ -1,4 +1,5 @@
 <template>
+  <editor-content :editor="editor" />
     <img src="/ui/icp_login.png" class="image_button" @click="authenticate_icp()" >
 </template>
 
@@ -9,7 +10,36 @@ import { Principal } from '@dfinity/principal';
 import { infinitinote_backend, canisterId, createActor } from '../../../declarations/infinitinote_backend';
 import { inject, onMounted, ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
+import { Editor } from '@tiptap/core'
+import Collaboration from '@tiptap/extension-collaboration'
+import CollaborationCursor from '@tiptap/extension-collaboration-cursor'
+import { HocuspocusProvider } from '@hocuspocus/provider'
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+// Set up the Hocuspocus WebSocket provider
+const provider = new HocuspocusProvider({
+  url: 'ws://0.0.0.0:80',
+  name: 'document',
+})
 
+const editor = useEditor({
+  extensions: [
+  StarterKit.configure({
+      // The Collaboration extension comes with its own history handling
+      history: false,
+    }),
+    Collaboration.configure({
+      document: provider.document,
+    }),
+    CollaborationCursor.configure({
+      provider: provider,
+      user: {
+        name: 'hira-'+new Date().toLocaleTimeString(),
+        color: '#f783ac',
+      },
+    }),
+  ],
+})
 var userAuthenticated = inject('userAuthenticated');
 var userPrincipal = inject('userPrincipal');
 var isLoading = inject('isLoading');
@@ -63,5 +93,11 @@ onMounted( async () => {
 </script>
 
 <style>
-
+.collaboration-cursor__label{
+    font-size: 18px !important;
+    width: 133px;
+    height: 24px;
+    border-radius: 30px;
+    padding: 10px;
+}
 </style>
